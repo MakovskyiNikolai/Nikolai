cdcd856318144327dddd0194870e0f0b5056af9b
MakovskyiNikolai <MakovskyiNikolai@users.noreply.github.com>
2019-04-23 16:45:08 +0300

    1st program

---
+++ src/main/java/sandbox/MyFirstProgram.java
@@ -0,0 +1,36 @@
+package sandbox;
+
+public class MyFirstProgram {
+
+  public static void main(String[] args) {         //функция main обратилась к функции hello
+    hello("world");
+    hello("user");
+    hello("user1");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+
+
+
+  }
+  public static void hello(String somebody){      //параметр или аргумент функции
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
+  }
+
+}
+
+
+
+//void не возвращает значение
